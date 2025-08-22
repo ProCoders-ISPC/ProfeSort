94960d85d9cb5d2b2d7af4b7cc1804e90b3115cc
Juan Pablo Sánchez Brandán <jpsanchezbrandan@gmail.com>
2025-08-22 00:59:37 -0300

    fix: se realizaron correciones al portfolio personal de Juan Pablo Sánchez

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] // ← corregido aquí
 })
 export class App {
   protected readonly title = signal('profesort-frontend');
