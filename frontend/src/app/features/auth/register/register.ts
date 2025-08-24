ebe55bfa98bd5fc5fa274551c0ba4a4d5419a9ee
Cristian Isaac Vargas <cristian.v62@gmail.com>
2025-08-24 13:12:05 -0300

    Refactor: Se creo el componente para el boton de volver al inicio se lo enruto correctamente al home y se corrigieron detalles de estilo de los formularios

--- frontend/src/app/features/auth/register/register.ts
+++ frontend/src/app/features/auth/register/register.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { BackToHomeButtonComponent } from '../../../shared/components/buttons/back-to-home-button/back-to-home-button';
 import { BackgroundComponent } from '../../../shared/components/background/background';
-
 @Component({
   selector: 'app-register',
-  imports: [BackgroundComponent],
+  imports: [BackToHomeButtonComponent, BackgroundComponent,],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
-
-}
+export class Register {}
