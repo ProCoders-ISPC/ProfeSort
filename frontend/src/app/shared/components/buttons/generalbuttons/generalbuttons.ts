98bd60d6a8d1e22a65d5038b7985188bbf56d4dd
lauzarg <lauzarate@mi.unc.edu.ar>
2025-08-30 08:25:19 -0300

    fix: se comienza la reorganizacion de carpetas y migrado del css a sus componentes correspondientes

---
+++ frontend/src/app/shared/components/buttons/generalbuttons/generalbuttons.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'generalbuttons',
+  imports: [],
+  templateUrl: './generalbuttons.html',
+  styleUrl: './generalbuttons.css'
+})
+export class Generalbuttons {
+
+}
