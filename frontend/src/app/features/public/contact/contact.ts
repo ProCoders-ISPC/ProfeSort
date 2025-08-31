ecd206b605d78d14e16f54911f212de66a959182
Karinaquinteros <karinaq38@gmail.com>
2025-08-31 16:35:28 -0300

    fix: se hace reactivo form de contacto

--- frontend/src/app/features/public/contact/contact.ts
+++ frontend/src/app/features/public/contact/contact.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrls: ['./contact.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', Validators.required]
+    });
+  }
+
   onSubmit() {
-    console.log('Formulario enviado!');
+    if (this.contactForm.valid) {
+      
+      console.log(this.contactForm.value);
+    }
   }
 }
