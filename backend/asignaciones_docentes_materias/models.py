9c97e962870f7f20208c5bb4456c5df0e6a18c0f
lauzarg <lauzarate@mi.unc.edu.ar>
2025-09-27 16:29:33 -0300

    feat: se migra asignaciones_docentes_materias a la bdd

--- backend/asignaciones_docentes_materias/models.py
+++ backend/asignaciones_docentes_materias/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class AsignacionDocenteMateria(models.Model):
+    id_asignacion = models.AutoField(primary_key=True)
+    id_rol = models.IntegerField()
+    id_materia = models.IntegerField()
+    fecha_asignacion = models.DateField(auto_now_add=True)
+    estado = models.CharField(max_length=50, default='Activa')
+
+    class Meta:
+        db_table = 'asignaciones_docentes_materias'
+
+
