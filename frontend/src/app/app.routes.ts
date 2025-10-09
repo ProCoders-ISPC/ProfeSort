c6321df361c985880b9cb83fa862edc22cba591c
Karinaquinteros <karinaq38@gmail.com>
2025-10-09 20:42:47 -0300

    refactor: se elimina componente portafolio personal y se coloca enlace externo an about

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { Register } from './features/auth/register/register';
 import { About } from './features/public/about/about';
 import { ContactComponent } from './features/public/contact/contact';
 import { LayoutComponent } from './features/public/layout/layout';
-import { KarinaQuinteros } from './features/public/portfolios/karina-quinteros/karina-quinteros.component';
 import { JuanpabloSanchez } from './features/public/portfolios/juanpablo-sanchez/juanpablo-sanchez'
 import { JuanignacioGioda } from './features/public/portfolios/juanignacio-gioda/juanignacio-gioda';
 import { LauraZarateComponent as LauraZarate } from './features/public/portfolios/laura-zarate/laura-zarate.component';
@@ -26,7 +25,6 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 
-  { path: 'portfolio/karina-quinteros', component: KarinaQuinteros },
   { path: 'portfolio/laura-zarate', component: LauraZarate },
   { path: 'portfolio/juanpablo-sanchez', component: JuanpabloSanchez },
   { path: 'portfolio/juanignacio-gioda', component: JuanignacioGioda },
