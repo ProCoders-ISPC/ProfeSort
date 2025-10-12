017dfebeb1050be7b415a78facb23c5412fc7106
Juan Pablo Sánchez Brandán <jpsanchezbrandan@gmail.com>
2025-10-12 19:20:27 -0300

    refactor: adjunté mi portfolio alojado externamente al about del proyecto, también eliminé mi carpeta personal para evitar conflictos y código innecesario

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Register } from './features/auth/register/register';
 import { About } from './features/public/about/about';
 import { ContactComponent } from './features/public/contact/contact';
 import { LayoutComponent } from './features/public/layout/layout';
-import { JuanpabloSanchez } from './features/public/portfolios/juanpablo-sanchez/juanpablo-sanchez'
+
 import { JuanignacioGioda } from './features/public/portfolios/juanignacio-gioda/juanignacio-gioda';
 import { DanielPaezComponent } from './features/public/portfolios/daniel-paez/daniel-paez';
 
@@ -24,17 +24,16 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 
-  { path: 'portfolio/juanpablo-sanchez', component: JuanpabloSanchez },
   { path: 'portfolio/juanignacio-gioda', component: JuanignacioGioda },
   { path: 'portfolio/daniel-paez', component: DanielPaezComponent },
 
-  // RUTAS ADMIN (lazy loading - se cargará desde admin.routes.ts)
+
   {
     path: 'admin',
     loadChildren: () => import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES)
   },
 
-  // RUTAS DOCENTE (lazy loading - se cargará desde docente.routes.ts)
+
   {
     path: 'docente',
     loadChildren: () => import('./features/docente/docente.routes').then(m => m.DOCENTE_ROUTES)
